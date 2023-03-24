d5b63720bf0f7e06f99622227af902a1660fa545
DALM1 <92697916+DALM1@users.noreply.github.com>
2023-03-24 21:40:13 +0100

    Update debug

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,78 @@
-mod server;
-mod client;
+use std::io::{self, Write};
+use std::net::TcpStream;
+use std::sync::mpsc::{Receiver, Sender};
+use std::sync::{Arc, Mutex};
+use std::thread;
 
-fn main() {
-    std::thread::spawn(|| {
-        server::main().unwrap();
-    });
+fn read_input(input_tx: Sender<String>) -> io::Result<()> {
+    loop {
+        let mut input = String::new();
+        io::stdin().read_line(&mut input)?;
+        input_tx.send(input.trim().to_string())?;
+    }
+}
 
-    client::main().unwrap();
+fn read_stream(stream: TcpStream, clients: Arc<Mutex<Vec<TcpStream>>>) -> io::Result<()> {
+    let mut reader = io::BufReader::new(&stream);
+    loop {
+        let mut line = String::new();
+        reader.read_line(&mut line)?;
+        let mut clients = clients.lock().unwrap();
+        for client in &mut *clients {
+            if client.peer_addr().unwrap() != stream.peer_addr().unwrap() {
+                client.write_all(line.as_bytes())?;
+            }
+        }
+    }
+}
+
+fn write_stream(
+    stream: TcpStream,
+    clients: Arc<Mutex<Vec<TcpStream>>>,
+    output_rx: Receiver<String>,
+) -> io::Result<()> {
+    loop {
+        let output = output_rx.recv().unwrap();
+        let mut clients = clients.lock().unwrap();
+        for client in &mut *clients {
+            if client.peer_addr().unwrap() != stream.peer_addr().unwrap() {
+                client.write_all(output.as_bytes())?;
+            }
+        }
+    }
+}
+
+fn main() -> io::Result<()> {
+    let listener = std::net::TcpListener::bind("127.0.0.1:5000")?;
+
+    let clients: Arc<Mutex<Vec<TcpStream>>> = Arc::new(Mutex::new(Vec::new()));
+
+    for stream in listener.incoming() {
+        let clients = Arc::clone(&clients);
+
+        let mut stream = stream?;
+
+        let clients_clone = Arc::clone(&clients);
+        clients.lock().unwrap().push(stream.try_clone()?);
+
+        let (input_tx, input_rx) = std::sync::mpsc::channel();
+        let (output_tx, output_rx) = std::sync::mpsc::channel();
+
+        thread::spawn(move || read_stream(stream, clients_clone));
+        thread::spawn(move || write_stream(stream, clients_clone, output_rx));
+        thread::spawn(move || read_input(input_tx));
+
+        loop {
+            let input = input_rx.recv().unwrap();
+            if input == ":quit" {
+                break;
+            }
+            output_tx.send(format!("{}\n", input))?;
+        }
+
+        let mut clients = clients.lock().unwrap();
+        clients.retain(|client| client.peer_addr().unwrap() != stream.peer_addr().unwrap());
+    }
+
+    Ok(())
 }
--- src/server.rs
+++ src/server.rs
@@ -1,53 +1,67 @@
-use std::io::{Read, Write};
+use std::io::{BufRead, BufReader, Write};
 use std::net::{TcpListener, TcpStream};
+use std::sync::{Arc, Mutex};
 use std::thread;
 
-fn handle_client(mut stream: TcpStream, clients: &mut Vec<TcpStream>) {
-    let mut buf;
-    loop {
-        buf = [0; 512];
-        match stream.read(&mut buf) {
-            Ok(n) => {
-                if n == 0 {
-                    return;
-                }
-                let message = String::from_utf8_lossy(&buf[..n]);
-                println!("{}", message);
-
-                // Send the message to all clients
-                for client in clients.iter_mut() {
-                    if client.peer_addr().unwrap() != stream.peer_addr().unwrap() {
-                        client.write_all(message.as_bytes()).unwrap();
-                    }
-                }
-            }
-            Err(_) => {
-                return;
-            }
-        }
-    }
-}
-
-pub fn main() -> std::io::Result<()> {
+fn listen() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:5000")?;
     println!("Server listening on port 5000");
 
-    let mut clients = Vec::new();
+    let clients: Arc<Mutex<Vec<TcpStream>>> = Arc::new(Mutex::new(Vec::new()));
 
     for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                let peer_addr = stream.peer_addr().unwrap();
-                println!("New connection from {}", peer_addr);
-                clients.push(stream.try_clone()?);
-                thread::spawn(|| {
-                    handle_client(stream, &mut clients);
-                });
+        let stream = stream.expect("Failed to establish connection");
+        let clients = clients.clone();
+
+        let mut stream_clone = stream.try_clone().expect("Failed to clone stream");
+
+        thread::spawn(move || {
+            let username = get_username(&mut stream_clone).expect("Failed to get username");
+            let message = format!("{} has joined the chat", username);
+            broadcast_message(&clients, &message, None);
+
+            clients.lock().unwrap().push(stream_clone);
+
+            loop {
+                let message = read_message(&mut stream_clone).expect("Failed to read message");
+                let message = format!("{}: {}", username, message);
+                broadcast_message(&clients, &message, Some(&stream_clone));
             }
-            Err(e) => {
-                println!("Error: {}", e);
+        });
+    }
+
+    Ok(())
+}
+
+fn read_message(stream: &mut TcpStream) -> std::io::Result<String> {
+    let mut reader = BufReader::new(stream);
+    let mut message = String::new();
+    reader.read_line(&mut message)?;
+    Ok(message.trim().to_string())
+}
+
+fn get_username(stream: &mut TcpStream) -> std::io::Result<String> {
+    let mut reader = BufReader::new(stream);
+    let mut username = String::new();
+    write!(stream, "Enter your username: ")?;
+    reader.read_line(&mut username)?;
+    Ok(username.trim().to_string())
+}
+
+fn broadcast_message(clients: &Arc<Mutex<Vec<TcpStream>>>, message: &str, sender: Option<&TcpStream>) {
+    let clients = clients.lock().unwrap();
+    for client in clients.iter() {
+        if let Some(sender) = sender {
+            if *client == *sender {
+                continue;
             }
         }
+        let mut client_clone = client.try_clone().expect("Failed to clone client");
+        writeln!(client_clone, "{}", message).expect("Failed to send message to client");
     }
+}
+
+fn main() -> std::io::Result<()> {
+    listen()?;
     Ok(())
 }
