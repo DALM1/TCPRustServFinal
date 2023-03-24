6c775902a26c39fa66b45647793e34d88b39f31e
DALM1 <92697916+DALM1@users.noreply.github.com>
2023-03-24 21:51:20 +0100

    Update debug

--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,57 @@
-use std::io::{self, Write};
+use std::io::{self, BufRead, Write};
 use std::net::TcpStream;
-use std::sync::mpsc::{Receiver, Sender};
-use std::sync::{Arc, Mutex};
+use std::sync::mpsc::{self, TryRecvError};
 use std::thread;
 
-fn read_input(input_tx: Sender<String>) -> io::Result<()> {
-    loop {
-        let mut input = String::new();
-        io::stdin().read_line(&mut input)?;
-        input_tx.send(input.trim().to_string())?;
-    }
-}
+const LOCAL: &str = "127.0.0.1:5000";
+const MSG_SIZE: usize = 32;
 
-fn read_stream(stream: TcpStream, clients: Arc<Mutex<Vec<TcpStream>>>) -> io::Result<()> {
-    let mut reader = io::BufReader::new(&stream);
-    loop {
-        let mut line = String::new();
-        reader.read_line(&mut line)?;
-        let mut clients = clients.lock().unwrap();
-        for client in &mut *clients {
-            if client.peer_addr().unwrap() != stream.peer_addr().unwrap() {
-                client.write_all(line.as_bytes())?;
+fn main() {
+    let mut client = TcpStream::connect(LOCAL).expect("Failed to connect to server.");
+
+    println!("Enter your username:");
+    let mut username = String::new();
+    io::stdin()
+        .read_line(&mut username)
+        .expect("Failed to read input.");
+    let username = username.trim();
+
+    let (tx, rx) = mpsc::channel::<String>();
+
+    thread::spawn(move || loop {
+        let mut buf = vec![0; MSG_SIZE];
+        match client.read_exact(&mut buf) {
+            Ok(_) => {
+                let msg = buf.into_iter().take_while(|&x| x != 0).collect::<Vec<_>>();
+                println!("{}", String::from_utf8_lossy(&msg));
             }
-        }
-    }
-}
-
-fn write_stream(
-    stream: TcpStream,
-    clients: Arc<Mutex<Vec<TcpStream>>>,
-    output_rx: Receiver<String>,
-) -> io::Result<()> {
-    loop {
-        let output = output_rx.recv().unwrap();
-        let mut clients = clients.lock().unwrap();
-        for client in &mut *clients {
-            if client.peer_addr().unwrap() != stream.peer_addr().unwrap() {
-                client.write_all(output.as_bytes())?;
-            }
-        }
-    }
-}
-
-fn main() -> io::Result<()> {
-    let listener = std::net::TcpListener::bind("127.0.0.1:5000")?;
-
-    let clients: Arc<Mutex<Vec<TcpStream>>> = Arc::new(Mutex::new(Vec::new()));
-
-    for stream in listener.incoming() {
-        let clients = Arc::clone(&clients);
-
-        let mut stream = stream?;
-
-        let clients_clone = Arc::clone(&clients);
-        clients.lock().unwrap().push(stream.try_clone()?);
-
-        let (input_tx, input_rx) = std::sync::mpsc::channel();
-        let (output_tx, output_rx) = std::sync::mpsc::channel();
-
-        thread::spawn(move || read_stream(stream, clients_clone));
-        thread::spawn(move || write_stream(stream, clients_clone, output_rx));
-        thread::spawn(move || read_input(input_tx));
-
-        loop {
-            let input = input_rx.recv().unwrap();
-            if input == ":quit" {
+            Err(ref err) if err.kind() == io::ErrorKind::WouldBlock => (),
+            Err(_) => {
+                println!("Connection with server was severed.");
                 break;
             }
-            output_tx.send(format!("{}\n", input))?;
         }
+        match rx.try_recv() {
+            Ok(msg) => {
+                let mut buf = msg.clone().into_bytes();
+                buf.resize(MSG_SIZE, 0);
+                client.write_all(&buf).expect("Failed to send message.");
+            }
+            Err(TryRecvError::Empty) => (),
+            Err(TryRecvError::Disconnected) => break,
+        }
+        thread::sleep(std::time::Duration::from_millis(100));
+    });
 
-        let mut clients = clients.lock().unwrap();
-        clients.retain(|client| client.peer_addr().unwrap() != stream.peer_addr().unwrap());
+    loop {
+        let mut msg = String::new();
+        io::stdin()
+            .read_line(&mut msg)
+            .expect("Failed to read input.");
+        let msg = msg.trim().to_owned();
+        if msg == ":quit" || tx.send(msg).is_err() {
+            break;
+        }
     }
-
-    Ok(())
+    println!("Bye bye!");
 }
