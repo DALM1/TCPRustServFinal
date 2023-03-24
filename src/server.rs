2f242ec820a85135298cabb98b0bc34cefbf7ae4
DALM1 <92697916+DALM1@users.noreply.github.com>
2023-03-24 21:52:37 +0100

    Updates

--- src/server.rs
+++ src/server.rs
@@ -1,71 +1,61 @@
-use std::io::{BufRead, BufReader, Write};
+use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
-use std::sync::{Arc, Mutex};
 use std::thread;
 
-fn listen() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:5000")?;
-    println!("Server listening on port 5000");
+fn handle_client(mut stream: TcpStream) -> std::io::Result<()> {
+    let mut buf = [0; 512];
+    let mut username = String::new();
 
-    let clients: Arc<Mutex<Vec<(String, TcpStream)>>> = Arc::new(Mutex::new(Vec::new()));
-
-    for stream in listener.incoming() {
-        let stream = stream.expect("Failed to establish connection");
-
-        let clients = Arc::clone(&clients);
-        let mut stream_clone = stream.try_clone().expect("Failed to clone stream");
-
-        thread::spawn(move || {
-            let username = get_username(&mut stream_clone).expect("Failed to get username");
-            let message = format!("{} has joined the chat", username);
-            broadcast_message(&clients, &message, None);
-
-            clients.lock().unwrap().push((username.clone(), stream_clone));
-
-            loop {
-                let message = read_message(&mut stream_clone).expect("Failed to read message");
-                let message = format!("{}: {}", username, message);
-                broadcast_message(&clients, &message, Some(&username));
-            }
-
-            let message = format!("{} has left the chat", username);
-            broadcast_message(&clients, &message, None);
-            clients.lock().unwrap().retain(|client| client.0 != username);
-        });
+    // Read username from the client
+    match stream.read(&mut buf) {
+        Ok(n) => {
+            username = String::from_utf8_lossy(&buf[..n]).to_string();
+            println!("{} has joined the chat", username);
+        }
+        Err(e) => {
+            println!("Error reading from client: {}", e);
+            return Err(e);
+        }
     }
 
-    Ok(())
-}
-
-fn read_message(stream: &mut TcpStream) -> std::io::Result<String> {
-    let mut reader = BufReader::new(stream);
-    let mut message = String::new();
-    reader.read_line(&mut message)?;
-    Ok(message.trim().to_string())
-}
-
-fn get_username(stream: &mut TcpStream) -> std::io::Result<String> {
-    let mut reader = BufReader::new(stream);
-    let mut username = String::new();
-    write!(stream, "Enter your username: ")?;
-    reader.read_line(&mut username)?;
-    Ok(username.trim().to_string())
-}
-
-fn broadcast_message(clients: &Arc<Mutex<Vec<(String, TcpStream)>>>, message: &str, sender: Option<&String>) {
-    let clients = clients.lock().unwrap();
-    for client in clients.iter() {
-        if let Some(sender) = sender {
-            if client.0 == *sender {
-                continue;
+    loop {
+        let mut buf = [0; 512];
+        match stream.read(&mut buf) {
+            Ok(n) => {
+                if n == 0 {
+                    println!("{} has left the chat", username);
+                    return Ok(());
+                }
+                let message = String::from_utf8_lossy(&buf[..n]).to_string();
+                println!("{}: {}", username, message);
+            }
+            Err(e) => {
+                println!("Error reading from client: {}", e);
+                return Err(e);
             }
         }
-        let mut client_clone = client.1.try_clone().expect("Failed to clone client");
-        writeln!(client_clone, "{}", message).expect("Failed to send message to client");
     }
 }
 
 fn main() -> std::io::Result<()> {
-    listen()?;
+    let listener = TcpListener::bind("127.0.0.1:5000")?;
+    println!("Server started on port 5000");
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                // Spawn a new thread for each client connection
+                thread::spawn(move || {
+                    if let Err(e) = handle_client(stream) {
+                        println!("Error handling client: {}", e);
+                    }
+                });
+            }
+            Err(e) => {
+                println!("Error accepting client connection: {}", e);
+            }
+        }
+    }
+
     Ok(())
 }
