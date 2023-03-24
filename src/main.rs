e84bd69942aac5c1f144ef5fe3f65064c1ddab29
DALM1 <92697916+DALM1@users.noreply.github.com>
2023-03-24 22:12:26 +0100

    Updates

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,10 @@
-use std::io::{self, BufRead, Write};
-use std::net::TcpStream;
-use std::sync::mpsc::{self, TryRecvError};
-use std::thread;
-
-const LOCAL: &str = "127.0.0.1:5000";
-const MSG_SIZE: usize = 32;
+mod server;
+mod client;
 
 fn main() {
-    let mut client = TcpStream::connect(LOCAL).expect("Failed to connect to server.");
-
-    println!("Enter your username:");
-    let mut username = String::new();
-    io::stdin()
-        .read_line(&mut username)
-        .expect("Failed to read input.");
-    let username = username.trim();
-
-    let (tx, rx) = mpsc::channel::<String>();
-
-    thread::spawn(move || loop {
-        let mut buf = vec![0; MSG_SIZE];
-        match client.read_exact(&mut buf) {
-            Ok(_) => {
-                let msg = buf.into_iter().take_while(|&x| x != 0).collect::<Vec<_>>();
-                println!("{}", String::from_utf8_lossy(&msg));
-            }
-            Err(ref err) if err.kind() == io::ErrorKind::WouldBlock => (),
-            Err(_) => {
-                println!("Connection with server was severed.");
-                break;
-            }
-        }
-        match rx.try_recv() {
-            Ok(msg) => {
-                let mut buf = msg.clone().into_bytes();
-                buf.resize(MSG_SIZE, 0);
-                client.write_all(&buf).expect("Failed to send message.");
-            }
-            Err(TryRecvError::Empty) => (),
-            Err(TryRecvError::Disconnected) => break,
-        }
-        thread::sleep(std::time::Duration::from_millis(100));
+    std::thread::spawn(|| {
+        server::main().unwrap();
     });
 
-    loop {
-        let mut msg = String::new();
-        io::stdin()
-            .read_line(&mut msg)
-            .expect("Failed to read input.");
-        let msg = msg.trim().to_owned();
-        if msg == ":quit" || tx.send(msg).is_err() {
-            break;
-        }
-    }
-    println!("Bye bye!");
+    client::main().unwrap();
 }
--- src/server.rs
+++ src/server.rs
@@ -2,60 +2,40 @@ use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
 use std::thread;
 
-fn handle_client(mut stream: TcpStream) -> std::io::Result<()> {
-    let mut buf = [0; 512];
-    let mut username = String::new();
-
-    // Read username from the client
-    match stream.read(&mut buf) {
-        Ok(n) => {
-            username = String::from_utf8_lossy(&buf[..n]).to_string();
-            println!("{} has joined the chat", username);
-        }
-        Err(e) => {
-            println!("Error reading from client: {}", e);
-            return Err(e);
-        }
-    }
-
+fn handle_client(mut stream: TcpStream) {
+    let mut buf;
     loop {
-        let mut buf = [0; 512];
+        buf = [0; 512];
         match stream.read(&mut buf) {
             Ok(n) => {
                 if n == 0 {
-                    println!("{} has left the chat", username);
-                    return Ok(());
+                    return;
                 }
-                let message = String::from_utf8_lossy(&buf[..n]).to_string();
-                println!("{}: {}", username, message);
+                let message = String::from_utf8_lossy(&buf[..n]);
+                println!("{}", message);
             }
-            Err(e) => {
-                println!("Error reading from client: {}", e);
-                return Err(e);
+            Err(_) => {
+                return;
             }
         }
     }
 }
 
-fn main() -> std::io::Result<()> {
+pub fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:5000")?;
-    println!("Server started on port 5000");
+    println!("Server listening on port 5000");
 
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                // Spawn a new thread for each client connection
-                thread::spawn(move || {
-                    if let Err(e) = handle_client(stream) {
-                        println!("Error handling client: {}", e);
-                    }
+                thread::spawn(|| {
+                    handle_client(stream);
                 });
             }
             Err(e) => {
-                println!("Error accepting client connection: {}", e);
+                println!("Error: {}", e);
             }
         }
     }
-
     Ok(())
 }
