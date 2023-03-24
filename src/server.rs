6a0685b6d822eb96fa4b2378ee9cd7a4408cc004
DALM1 <92697916+DALM1@users.noreply.github.com>
2023-03-24 21:35:49 +0100

    Update debug

--- src/server.rs
+++ src/server.rs
@@ -14,7 +14,7 @@ fn handle_client(mut stream: TcpStream, clients: &mut Vec<TcpStream>) {
                 let message = String::from_utf8_lossy(&buf[..n]);
                 println!("{}", message);
 
-               
+                // Send the message to all clients
                 for client in clients.iter_mut() {
                     if client.peer_addr().unwrap() != stream.peer_addr().unwrap() {
                         client.write_all(message.as_bytes()).unwrap();
@@ -28,7 +28,6 @@ fn handle_client(mut stream: TcpStream, clients: &mut Vec<TcpStream>) {
     }
 }
 
-
 pub fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:5000")?;
     println!("Server listening on port 5000");
@@ -38,6 +37,8 @@ pub fn main() -> std::io::Result<()> {
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
+                let peer_addr = stream.peer_addr().unwrap();
+                println!("New connection from {}", peer_addr);
                 clients.push(stream.try_clone()?);
                 thread::spawn(|| {
                     handle_client(stream, &mut clients);
@@ -50,19 +51,3 @@ pub fn main() -> std::io::Result<()> {
     }
     Ok(())
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
